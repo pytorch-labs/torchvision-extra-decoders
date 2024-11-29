754ea95127c49be587d3c3fca48cc25c3029f42f
Nicolas Hug <contact@nicolas-hug.com>
2024-11-29 15:37:17 +0000

    Try to run auditwheel

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -2,7 +2,14 @@
 
 set -ex
 
-old="linux_x86_64"
-new="manylinux_2_17_x86_64.manylinux2014_x86_64"
-echo "Replacing ${old} with ${new} in wheel name"
-mv dist/*${old}*.whl $(echo dist/*${old}*.whl | sed "s/${old}/${new}/")
+ls dist
+wheel_path=`find dist -type f -name "*.whl"`
+
+echo Found $wheel_path
+
+auditwheel -v repair --plat manylinux_2_38_x86_64  $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
+
+# old="linux_x86_64"
+# new="manylinux_2_17_x86_64.manylinux2014_x86_64"
+# echo "Replacing ${old} with ${new} in wheel name"
+# mv dist/*${old}*.whl $(echo dist/*${old}*.whl | sed "s/${old}/${new}/")
