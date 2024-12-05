a8f34f61dde8799287e5bb2e34a034ff368f286f
Nicolas Hug <contact@nicolas-hug.com>
2024-12-05 13:35:13 +0000

    DEBUG

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -7,6 +7,8 @@ wheel_path=`find dist -type f -name "*.whl"`
 
 echo Found $wheel_path
 
+echo $CONDA_PREFIX
+
 ${CONDA_RUN} auditwheel -v repair --plat manylinux_2_38_x86_64  $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
 
 # old="linux_x86_64"
