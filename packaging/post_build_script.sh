d3701d8283eaa7da2fb5f55363860ff0ebb5bd97
Nicolas Hug <contact@nicolas-hug.com>
2024-12-05 14:20:32 +0000

    Try something

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -7,11 +7,16 @@ wheel_path=`find dist -type f -name "*.whl"`
 
 echo Found $wheel_path
 
-echo CONDA_PREFIX = $CONDA_PREFIX
-echo `ls $CONDA_PREFIX/lib`
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
-${CONDA_RUN} auditwheel -v repair --plat manylinux2014_x86_64 $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
+# Put all the .so files we want to ship with the wheels within the lib_to_bundle
+# dir. Maybe it's enough to LD_LIBRARY_PATH $CONDA_PREFIX/lib but I fear this
+# might include some un-desired libc or libcxx so files?
+mkdir libs_to_bundle
+for f in `find $CONDA_PREFIX/lib | grep -e libavif.so -e libdav1d.so -e librav1e.so  -e libaom.so -e libheif.so -e libx265.so -e libde265.so`; do cp $f libs_to_bundle; done
+echo `ls libs_to_bundle`
+export LD_LIBRARY_PATH="$PWD/libs_to_bundle:$LD_LIBRARY_PATH"
+
+${CONDA_RUN} auditwheel -v repair --plat manylinux_2_38_x86_64  $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
 
 # mv original wheel to a different dir, the 'repaired' wheel outputed by auditwheel should still be in `dist` and treated and *the* wheel
 mkdir original_wheel
