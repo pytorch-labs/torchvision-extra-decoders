c209e49964c1900eabba9c6e25e6c9235bdce415
Nicolas Hug <contact@nicolas-hug.com>
2024-12-05 13:53:39 +0000

    DEBUG

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -13,6 +13,10 @@ export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
 ${CONDA_RUN} auditwheel -v repair --plat manylinux_2_38_x86_64  $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
 
+# mv original wheel to a different dir, the 'repaired' wheel outputed by auditwheel should still be in `dist` and treated and *the* wheel
+mkdir original_wheel
+mv $wheel_path original_wheel/
+
 # old="linux_x86_64"
 # new="manylinux_2_17_x86_64.manylinux2014_x86_64"
 # echo "Replacing ${old} with ${new} in wheel name"
