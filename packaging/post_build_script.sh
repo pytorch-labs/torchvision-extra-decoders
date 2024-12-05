c8fd2d15e1efd39705b797d59a96f67c1607b6b1
Nicolas Hug <contact@nicolas-hug.com>
2024-12-05 15:15:16 +0000

    eww

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -18,11 +18,14 @@ export LD_LIBRARY_PATH="$PWD/libs_to_bundle:$LD_LIBRARY_PATH"
 
 ${CONDA_RUN} auditwheel -v repair --plat manylinux_2_34_x86_64 $wheel_path  --exclude libtorch_python.so --exclude libc10.so --exclude libtorch.so --exclude libtorch_cpu.so --wheel-dir dist
 
-# mv original wheel to a different dir, the 'repaired' wheel outputed by auditwheel should still be in `dist` and treated and *the* wheel
+# mv original wheel to a different dir, the 'repaired' wheel outputed by
+# auditwheel is still in dist/
 mkdir original_wheel
 mv $wheel_path original_wheel/
 
-# old="linux_x86_64"
-# new="manylinux_2_17_x86_64.manylinux2014_x86_64"
-# echo "Replacing ${old} with ${new} in wheel name"
-# mv dist/*${old}*.whl $(echo dist/*${old}*.whl | sed "s/${old}/${new}/")
+
+# This is absolutely disgusting.
+old="manylinux_2_34_x86_64"
+new="manylinux_2_17_x86_64.manylinux2014_x86_64"
+echo "Replacing ${old} with ${new} in wheel name"
+mv dist/*${old}*.whl $(echo dist/*${old}*.whl | sed "s/${old}/${new}/")
